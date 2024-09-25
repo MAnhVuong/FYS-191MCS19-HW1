d620f8457ee82f1822b3e318b7755cd643e4cec5
MAnhVuong <vuongminhanh2006@gmail.com>
2024-09-25 16:09:24 -0400

    Add files via upload

---
+++ fraction_division.py
@@ -0,0 +1,8 @@
+# Author   : Anh Minh Vuong
+# Email    : minhanhvuong@umass.edu
+# Spire ID : 34892350
+
+dividend = int(input("What is the dividend: "))
+divisor = int(input("What is the divisor: "))
+print(str(dividend) + '/' + str(divisor), "equals:")
+print('\t' + str(dividend//divisor), "and", str(dividend%divisor) + '/' + str(divisor))
